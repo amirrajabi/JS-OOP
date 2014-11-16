405f388b0811c4b43b02936e1f91c6a60a77e145
Amir <amir@amirrajabi.com>
2014-11-16 21:49:56 +1100

    added classes

--- js/main.js
+++ js/main.js
@@ -7,34 +7,46 @@
 function milliSecound(){
     var date = new Date();
     return date.getMilliseconds();
-}
+};
 
 
 //class A
 function classA(){
+
     var date = new Date();
 
     return function(){
 
         return date.getMilliseconds();
 
-    }
+    };
 
-}
+};
 
 //class B
+//capsulated var and function
 function classB(){
-    var date = new Date();
 
-    var myNestedFun = function(){
-
-        return date.getMilliseconds();
-
-    };
+    var date = new Date(),
+        myNestedFun = function(){
+            return date.getMilliseconds();
+        };
 
     return{
         foo : myNestedFun
     };
 
-}
+};
+
+
+
+window.onload = function(){
+    var output = document.getElementById('output');
+    var getMillA = classA();
+    var getMillB = new classB();
+
+    output.innerHTML = milliSecound();
+    output.innerHTML += '<br/>'+ getMillA();
+    output.innerHTML += '<br/>'+ getMillB.foo();
+};
 
